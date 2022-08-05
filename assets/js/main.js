e33894597ed56cd7c5713455061a169b6c0d4f53
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-08-05 16:55:19 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+
+function calculateIMC() {
+    
+    const weightInput = Number(document.querySelector('#weightInput').value);
+    const heightInput = Number(document.querySelector('#heightInput').value);
+    let result = document.querySelector('#result');
+    const IMC = weightInput / (heightInput**2);
+
+    function tipoIMC() {
+        if (IMC < 18.5) {
+            return 'abaixo do peso';
+        } else if (IMC < 25) {
+            return 'com peso normal'
+        } else if (IMC < 30) {
+            return 'com sobrepeso'
+        }else if (IMC < 35) {
+            return 'com obesidade grau 1'
+        } else if (IMC < 40) {
+            return 'com obesidade grau 2'
+        } else { //acima de 40kg
+            return 'com obesidade grau 3'
+        }
+    };
+
+    function invalidResult() {
+        result.classList.add('invalid-result');
+        result.classList.remove('valid-result')
+    }
+
+    function validResult() {
+        result.classList.add('valid-result');
+        result.classList.remove('invalid-result')
+    }
+
+    if (!weightInput) {
+        result.innerHTML = 'peso inválido';
+        invalidResult()
+    } else if (!heightInput) {
+        result.innerHTML = 'altura inválida';
+        invalidResult()
+    } else {
+        result.innerHTML = `Seu IMC é: ${IMC.toFixed(2)}
+        <p>você está ${tipoIMC()}</p>`;
+        validResult()
+    }
+}
